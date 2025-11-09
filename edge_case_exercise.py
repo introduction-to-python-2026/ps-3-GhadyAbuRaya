f4220fc17d86cbc77f6123c20747ef28715e6643
GhadyAbuRaya <vxytjghx7d@privaterelay.appleid.com>
2025-11-09 17:52:51 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -5,11 +5,11 @@ def move(my_list, direction):
 
   
     if direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
+        my_list[index_of_one] 
+        my_list[index_of_one + 1] 
 
     elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
+        my_list[index_of_one] 
+        my_list[index_of_one - 1] 
 
     return my_list
